81eacafe2555e38cc663aca2e6940b620a1db264
Denis Lino Puma Ticona <dpumaticona@gmail.com>
2025-04-02 02:29:37 -0500

    Correcciones3

---
+++ dist/assets/index-DYUFzAHe.js
@@ -0,0 +1,1 @@
+import{a6 as a}from"./index-ByPSuvDA.js";const e="modules",o=async r=>{try{return(await a().post(`${e}/curriculum`,r)).data.data}catch{return[]}},c=async(r,t)=>{try{return(await a().get(`${e}/${r}/curriculum/${t}`)).data.data}catch{return{}}};export{c as _,o as a};
